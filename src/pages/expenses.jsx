b2b93baeec6538f057ecdf43ae07faa30986b738
GabrielIndrawan <111202213979@mhs.dinus.ac.id>
2024-11-04 17:22:14 +0700

    Menambahkan page Expenses 2

--- src/pages/expenses.jsx
+++ src/pages/expenses.jsx
@@ -7,7 +7,7 @@ const ExpensesPage = () => {
             <div className="flex flex-row mb-5">
                 <div className="text-xl text-gray-500">Expenses Comparison</div>
             </div>
-            <div className="w-full mb-5">
+            <div className="w-full mb-5 h-[210px]">
                 <Card untitled height="[210px]">
                 </Card>
             </div>
@@ -16,32 +16,32 @@ const ExpensesPage = () => {
             </div>
             <div className="grid grid-cols-3 space-x-4">
                 <div className="flex flex-col space-y-5">
-                    <div className="w-auto h-[160px]">
+                    <div className="w-auto ">
                         <Card untitled height="[160px]">
                             Lorem Ipsum Lorem Ipsum
                         </Card>
                     </div>
-                    <div className="w-auto h-[160px]">
+                    <div className="w-auto ">
                         <Card untitled height="[160px]">
                         </Card>
                     </div>
                 </div>
                 <div className="flex flex-col space-y-5">
-                    <div className="w-auto h-[160px]">
+                    <div className="w-auto ">
                         <Card untitled height="[160px]">
                         </Card>
                     </div>
-                    <div className="w-auto h-[160px]">
+                    <div className="w-auto ">
                         <Card untitled height="[160px]">
                         </Card>
                     </div>
                 </div>
                 <div className="flex flex-col space-y-5">
-                    <div className="w-auto h-[160px]">
+                    <div className="w-auto ">
                         <Card untitled height="[160px]">
                         </Card>
                     </div>
-                    <div className="w-auto h-[160px]">
+                    <div className="w-auto ">
                         <Card untitled height="[160px]">
                         </Card>
                     </div>
